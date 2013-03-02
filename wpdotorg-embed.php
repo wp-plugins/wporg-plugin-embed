2111b8dc9a29350e3f6e2a526c7e738123a732c5
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-02 08:56:17 +0000

    Releasing 1.1
    git-svn-id: https://plugins.svn.wordpress.org/wporg-plugin-embed/trunk@675301 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpdotorg-embed.php
+++ wpdotorg-embed.php
@@ -4,7 +4,7 @@
 Plugin Name: WP.org Embed
 Plugin URI: http://www.leewillis.co.uk/wordpress-plugins
 Description: Paste the URL to a WordPress.org plugin into your posts or pages, and have the plugin information pulled in and displayed automatically
-Version: 1.0
+Version: 1.1
 Author: Lee Willis
 Author URI: http://www.leewillis.co.uk/
 */
@@ -33,7 +33,7 @@ Author URI: http://www.leewillis.co.uk/
 
 /**
  * This class handles being the oEmbed provider in terms of registering the URLs that
- * we can embed, and handling the actual oEmbed calls. It relies on the WP.org API 
+ * we can embed, and handling the actual oEmbed calls. It relies on the WP.org API
  * class to retrieve the information from WordPress.org
  * @uses class wpdotorg_api
  */
@@ -56,7 +56,7 @@ class wpdotorg_embed {
 		add_action ( 'init', array ( $this, 'register_oembed_handler' ) );
 		add_action ( 'init', array ( $this, 'maybe_handle_oembed' ) );
 		add_action ( 'wp_enqueue_scripts', array ( $this, 'enqueue_styles' ) );
-		
+
 		// @TODO i18n
 
 	}
@@ -71,7 +71,7 @@ class wpdotorg_embed {
 
 		wp_register_style ( 'wpdotorg-embed', plugins_url(basename(dirname(__FILE__)).'/css/wpdotorg-embed.css' ) );
         wp_enqueue_style ( 'wpdotorg-embed' );
-	
+
 	}
 
 
@@ -89,6 +89,7 @@ class wpdotorg_embed {
 		$key = $this->get_key();
 		$oembed_url = add_query_arg ( array ( 'wpdotorg_oembed' => $key ), $oembed_url);
 		wp_oembed_add_provider ( '#https?://wordpress.org/extend/plugins/.*/?#i', $oembed_url, true );
+		wp_oembed_add_provider ( '#https?://wordpress.org/extend/themes/.*/?#i', $oembed_url, true );
 
 	}
 
@@ -102,7 +103,7 @@ class wpdotorg_embed {
 	private function get_key() {
 
 		$key = get_option ( 'wpdotorg_oembed_key' );
-		
+
 		if ( ! $key ) {
 			$key = md5 ( time() . rand ( 0,65535 ) );
 			add_option ( 'wpdotorg_oembed_key', $key, '', 'yes' );
@@ -149,21 +150,28 @@ class wpdotorg_embed {
 			die ( 'Only json here, probably #blamenacin' );
 		}
 
-		preg_match ( '#https?://wordpress.org/extend/plugins/([^/]*)/?$#i', $url, $matches );
+		if ( preg_match ( '#https?://wordpress.org/extend/plugins/([^/]*)/?$#i', $url, $matches ) ) {
 
-		if ( ! $url || empty ( $matches[1] ) ) {
-			header ( 'HTTP/1.0 404 Not Found' );
+			$this->oembed_wpdotorg_plugin ( $matches[1] );
+
+		} elseif ( preg_match ( '#https?://wordpress.org/extend/themes/([^/]*)/?$#i', $url, $matches ) ) {
+
+			$this->oembed_wpdotorg_theme ( $matches[1] );
+
+		} else {
+
+        	header ( 'HTTP/1.0 404 Not Found' );
 			die ( 'Mike Little is lost, and afraid' );
+
 		}
 
-		$this->oembed_wpdotorg_plugin ( $matches[1] );
 
 	}
 
 
 
 	/**
-	 * Retrieve the information from wpdotorg for a repo, and
+	 * Retrieve the information from wpdotorg for a plugin, and
 	 * output it as an oembed response
 	 */
 	private function oembed_wpdotorg_plugin ( $slug ) {
@@ -182,7 +190,7 @@ class wpdotorg_embed {
 		$response->html .= '<p><a href="http://wordpress.org/extend/plugins/'.esc_attr($slug).'" target="_blank"><strong>'.esc_html($plugin->name)."</strong></a><br/>";
 		if ( ! empty ( $plugin->author ) )
 		    $response->html .= 'by <span class="wpdotorg-embed-plugin-author">'.wp_kses_post($plugin->author).'</span></p>';
-		
+
 		if ( ! empty ( $plugin->sections['description'] ) )
 			$response->html .= '<p class="wpdotorg-embed-plugin-description">'.wp_kses_post($plugin->sections['description']).'</p>';
 
@@ -199,7 +207,59 @@ class wpdotorg_embed {
 		if ( ! empty ( $plugin->downloaded ) ) {
 			$stats .= '<li>Downloaded '.esc_html($plugin->downloaded).' times</li>';
 		}
-		
+
+		if ( ! empty ( $stats ) ) {
+			$response->html .= '<p><strong>Stats:</strong></p><ul class="wpdotorg-embed-stats-list">'.$stats.'</ul>';
+		}
+
+		$response->html .= '</div>';
+
+		header ( 'Content-Type: application/json' );
+		echo json_encode ( $response );
+		die();
+
+	}
+
+
+
+	/**
+	 * Retrieve the information from wpdotorg for a theme, and
+	 * output it as an oembed response
+	 */
+	private function oembed_wpdotorg_theme ( $slug ) {
+
+		$theme = $this->api->get_theme ( $slug );
+
+		$response = new stdClass();
+		$response->type = 'rich';
+		$response->width = '10';
+		$response->height = '10';
+		$response->version = '1.0';
+		$response->title = $theme->description;
+		$response->html = '<div class="wpdotorg-embed wpdotorg-embed-theme">';
+
+		// @TODO This should all be templated
+		$response->html .= '<p><a href="http://wordpress.org/extend/themes/'.esc_attr($slug).'" target="_blank"><strong>'.esc_html($theme->name)."</strong></a><br/>";
+		if ( ! empty ( $theme->author ) )
+		    $response->html .= 'by <span class="wpdotorg-embed-theme-author">'.wp_kses_post($theme->author).'</span></p>';
+
+		if ( ! empty ( $theme->sections['description'] ) )
+			$response->html .= '<p class="wpdotorg-embed-theme-description">'.wp_kses_post($theme->sections['description']).'</p>';
+
+		$stats = '';
+
+		if ( ! empty ( $theme->version) ) {
+			$stats .= '<li>Current version: '.esc_html($theme->version).'</li>';
+		}
+
+		if ( ! empty ( $theme->rating ) ) {
+			$stats .= '<li>Rating: '.esc_html($theme->rating).' ('.esc_html($theme->num_ratings).' ratings)</li>';
+		}
+
+		if ( ! empty ( $theme->downloaded ) ) {
+			$stats .= '<li>Downloaded '.esc_html($theme->downloaded).' times</li>';
+		}
+
 		if ( ! empty ( $stats ) ) {
 			$response->html .= '<p><strong>Stats:</strong></p><ul class="wpdotorg-embed-stats-list">'.$stats.'</ul>';
 		}
@@ -213,6 +273,8 @@ class wpdotorg_embed {
 	}
 
 }
+
+
 
 require_once ( 'wpdotorg-api.php' );
 
